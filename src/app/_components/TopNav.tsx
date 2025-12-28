d2a38ddf00079716c9b535cd4d03bdb88ff80994
AkramRamoul <ahmedakramramoul@gmail.com>
2025-12-28 08:23:12 +0100

    fixed next vulnerabilities

--- src/app/_components/TopNav.tsx
+++ src/app/_components/TopNav.tsx
@@ -1,11 +1,14 @@
 "use client";
 import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
 import { SimpleUploadButton } from "./uploadButton";
+import Link from "next/link";
 
 export default function TopNav() {
   return (
-    <nav className="flex h-20 w-full items-center justify-between border-b p-4 text-xl font-semibold">
-      <div>Gallery</div>
+    <nav className="flex h-20 w-full items-center justify-between border-b p-4 pr-20 text-xl font-semibold">
+      <div>
+        <Link href="/">Gallery</Link>
+      </div>
       <div className="flex flex-row items-center gap-4">
         <SignedOut>
           <SignInButton />
